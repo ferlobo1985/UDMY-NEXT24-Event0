062b4967dd01712d24de08f78334c52355b9da4c
ferlobo1985 <fernando.lobo.prez@gmail.com>
2023-12-17 23:00:43 -0300

    33-Showing Venue info
    33-Showing Venue info

--- src/components/events/venue_card.js
+++ src/components/events/venue_card.js
@@ -1,12 +1,28 @@
 'use client'
+import {
+    Card,
+    CardHeader,
+    CardBody,
+    Divider
+  } from "@nextui-org/react";
 
 export default function VenueCardComponent({venueData}){
 
-    console.log(venueData)
-
     return(
         <>
-            card
+            <Divider/>
+            <Card className="max-w-[400px] bg-slate-200 mt-5">
+                <CardHeader className="flex gap-3">
+                    <div className="flex flex-col">
+                        <p className="text-small">Venue information</p>
+                        <p className="text-xl">{venueData.name}</p>
+                    </div>
+                </CardHeader>
+                <Divider/>
+                <CardBody>
+                    <p>{venueData.address}, {venueData.state}</p>
+                </CardBody>
+            </Card>
         </>
     )
 
